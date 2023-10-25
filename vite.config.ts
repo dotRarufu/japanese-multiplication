11e502456b25ac6d979417fafab64076b76e3d58
dotRarufu <81721522+dotRarufu@users.noreply.github.com>
2023-10-25 11:50:25 +0800

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  name: 'Vedic Tricks',
+  short_name: 'Vedic Tricks',
+  theme_color: '#fcd662',
+  background_color: '#434148',
+  display: 'fullscreen',
+  scope: '/',
+  start_url: '/',
+  icons: [
+    {
+      src: 'manifest-icon-192.maskable.png',
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'any',
+    },
+    {
+      src: 'manifest-icon-192.maskable.png',
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'maskable',
+    },
+    {
+      src: 'manifest-icon-512.maskable.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any',
+    },
+    {
+      src: 'manifest-icon-512.maskable.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'maskable',
+    },
+  ],
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: './',
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
